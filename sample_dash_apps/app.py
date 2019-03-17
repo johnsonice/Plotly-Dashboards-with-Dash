21b97abb6a64b4f6f65e4375b3c3ae4a2f8559f1
Chengyu Huang <huangchengyu16@gmail.com>
2019-03-16 22:18:23 -0400

    finishing example

--- sample_dash_apps/app.py
+++ sample_dash_apps/app.py
@@ -1,6 +1,7 @@
 import base64
 import datetime
 import io
+import plotly.graph_objs as go
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
@@ -24,7 +25,21 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config.requests_pathname_prefix = ''
 
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 elements = [
+            html.H2(
+                children='SPR Review Document Topic Analysis',
+                style={
+                    'textAlign': 'center',
+                    'padding': '50px',
+                    #'color': colors['text']
+                }
+            ),
             dcc.Upload(
                     id='upload-data',
                     children=html.Div([
@@ -33,8 +48,8 @@ elements = [
                         ]),
                     style={
                         'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
+                        'height': '80px',
+                        'lineHeight': '80px',
                         'borderWidth': '1px',
                         'borderStyle': 'dashed',
                         'borderRadius': '5px',
@@ -44,7 +59,12 @@ elements = [
                     # Allow multiple files to be uploaded
                     multiple=True
                 ),
-            html.Div(id='output-data-upload',style={'width': '50%','margin': 'auto'}),
+            ## build the graph object 
+            dcc.Graph(id='topic-graph'),
+            ## build table object
+            html.Div(id='output-data-upload',style={'width': '50%',
+                                                    'margin': 'auto',
+                                                    'padding':'50px'}),
         ]
 
 app.layout = html.Div(elements)
@@ -96,19 +116,79 @@ def parse_doc(contents, filename, date,processor=processor):
     #res = html.Div([html.Div("{}".format(i)) for i in res])
     res = build_html_table(topic_df,filename,date)
     return res
-              
-@app.callback(Output('output-data-upload', 'children'),
+
+def create_graph(df,xaxis_name,yaxis_name):
+    df[xaxis_name]=df[xaxis_name].apply(lambda x: "Topic-"+str(x))
+    traces = [
+            go.Bar(x=df[xaxis_name],
+                   y=df[yaxis_name],
+                   text=df[xaxis_name])
+            ]
+    go_layout = go.Layout(title='Document Topic Distribution',
+                          xaxis={'title':xaxis_name,
+                                 'categoryorder': 'array',
+                                 'categoryarray': df[xaxis_name]},
+                          yaxis={'title':yaxis_name},
+                          hovermode='closest')
+    
+    res = {'data':traces,
+           'layout':go_layout}
+    return res
+
+def update_graph(contents, filename, date,processor=processor):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'docx' in filename.lower():
+            # Assume that the user uploaded a docx file
+            #res = read_doc(io.BytesIO(decoded))
+            doc = processor.read_doc(io.BytesIO(decoded))
+            ## get topic df
+            topic_df = get_topic_df(processor,doc)
+        else:
+            return html.Div([
+                'You much upload a word document. No other type of document is supported at this point.'
+            ])
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    print('chart create')
+    res = create_graph(topic_df,'gensim_topic','content_size') 
+    return res
+    
+#%%
+#@app.callback(Output('output-data-upload', 'children'),
+#              [Input('upload-data', 'contents')],
+#              [State('upload-data', 'filename'),
+#               State('upload-data', 'last_modified')])
+#def update_output(list_of_contents, list_of_names, list_of_dates):
+#    if list_of_contents is not None:
+#        children = [
+#            #parse_contents(c, n, d) for c, n, d in
+#            parse_doc(c, n, d) for c, n, d in
+#            zip(list_of_contents, list_of_names, list_of_dates)]
+#        return children
+
+#%%
+@app.callback(Output('topic-graph', 'figure'),
               [Input('upload-data', 'contents')],
               [State('upload-data', 'filename'),
                State('upload-data', 'last_modified')])
-def update_output(list_of_contents, list_of_names, list_of_dates):
+def update_output_graph(list_of_contents, list_of_names, list_of_dates):
+    children = None
     if list_of_contents is not None:
         children = [
             #parse_contents(c, n, d) for c, n, d in
-            parse_doc(c, n, d) for c, n, d in
+            update_graph(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+    if len(children) > 0:
+        children = children[0]
+    return children
 
+    
 
 if __name__ == '__main__':
     #app.run_server(port=8888, host='0.0.0.0', debug=True)
--- sample_dash_apps/dashboard/evaluate.py
+++ sample_dash_apps/dashboard/evaluate.py
@@ -11,6 +11,8 @@ from process_util import Processor
 import config
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import plotly.offline as pyo
 
 #%%
 def merge_transform_topic_df(topic_model_res,df_doc):
@@ -45,7 +47,27 @@ def get_topic_df(processor,doc):
     df_res.reset_index(level=0, inplace=True)
     df_res.sort_values(by='content_size', ascending=False,inplace=True)
     return df_res
+
+def create_graph(df,xaxis_name,yaxis_name):
     
+#    df[xaxis_name]=df[xaxis_name].astype(str)
+    df[xaxis_name]=df[xaxis_name].apply(lambda x: "Topic-"+str(x))
+    traces = [
+            go.Bar(x=df[xaxis_name],
+                   y=df[yaxis_name],
+                   text=df[xaxis_name])
+            ]
+    go_layout = go.Layout(title='Document Topic Distribution',
+                          xaxis={'title':xaxis_name,
+                                 'categoryorder': 'array',
+                                 'categoryarray': df[xaxis_name]},
+                          yaxis={'title':yaxis_name},
+                          hovermode='closest')
+    
+    res = {'data':traces,
+           'layout':go_layout}
+    return res
+
 #%%
 if __name__ == "__main__":
     ## initialize processor
@@ -56,3 +78,11 @@ if __name__ == "__main__":
     doc = processor.read_doc('test/Brazil_2013.DOCX')
     topic_df = get_topic_df(processor,doc)
     topic_df['content_size'].plot(kind='bar')
+    #plotly chart
+    fig_data = create_graph(topic_df,'gensim_topic','content_size')
+    pyo.plot(fig_data,filename='test/test.html')
+    
+    
+    
+    
+    
